4fa515f3702c290396ee0c3fe9f611aaec5c3d18
setiyawan12 <yayangsetiyawan0@gmail.com>
2020-09-19 05:56:54 +0700

    kotlin

--- app/src/main/java/com/example/smoot/MainActivity.kt
+++ app/src/main/java/com/example/smoot/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.smoot
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
@@ -26,44 +25,41 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setUpBottomNav()
+    }
+    fun setUpBottomNav(){
         fm.beginTransaction().add(R.id.container, fragmentHome).show(fragmentHome).commit()
         fm.beginTransaction().add(R.id.container, fragmentAkun).hide(fragmentAkun).commit()
-        fm.beginTransaction().add(R.id.container, fragmentKeranjang).hide(fragmentKeranjang).commit()
+        fm.beginTransaction().add(R.id.container, fragmentKeranjang).hide(fragmentKeranjang)
+            .commit()
 
         bottomNavigationView = findViewById(R.id.nav_view)
-        menu =bottomNavigationView.menu
-        menuItem =menu.getItem(0)
-        menuItem.isChecked=true
+        menu = bottomNavigationView.menu
+        menuItem = menu.getItem(0)
+        menuItem.isChecked = true
 
         bottomNavigationView.setOnNavigationItemReselectedListener { item ->
-            when(item.itemId){
-                R.id.navigation_home ->{
-                    Log.d("respons","home")
-                    menuItem =menu.getItem(0)
-                    menuItem.isChecked=true
-                    fm.beginTransaction().hide(active).show(fragmentHome).commit()
-                    active = fragmentHome
+            when (item.itemId) {
+                R.id.navigation_home -> {
+                    callFragment(0, fragmentHome)
                 }
-                R.id.navigation_keranjang ->{
-                    Log.d("respons","keranjang")
-                    menuItem =menu.getItem(1)
-                    menuItem.isChecked=true
-                    fm.beginTransaction().hide(active).show(fragmentKeranjang).commit()
-                    active = fragmentKeranjang
+                R.id.navigation_keranjang -> {
+                    callFragment(1, fragmentKeranjang)
+
                 }
-                R.id.navigation_akun ->{
-                    Log.d("respons","akun")
-                    menuItem =menu.getItem(2)
-                    menuItem.isChecked=true
-                    fm.beginTransaction().hide(active).show(fragmentAkun).commit()
-                    active = fragmentAkun
+                R.id.navigation_akun -> {
+                    callFragment(2, fragmentAkun)
 
                 }
             }
             false
         }
-
+    }
+    fun callFragment(int:Int, fragment: Fragment){
+        menuItem = menu.getItem(int)
+        menuItem.isChecked = true
+        fm.beginTransaction().hide(active).show(fragment).commit()
+        active = fragment
+    }
 
     }
-}
--- app/src/main/java/com/example/smoot/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/smoot/fragment/HomeFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager.widget.ViewPager
 import com.example.smoot.R
+import com.example.smoot.adapter.AdapterProduk
+import com.example.smoot.adapter.AdapterSlider
+import com.example.smoot.model.Produk
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,6 +24,8 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
+    lateinit var  vpSlider: ViewPager
+    lateinit var rvProduk: RecyclerView
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +43,41 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+//        return inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
+        vpSlider = view.findViewById(R.id.vp_slider)
+        rvProduk = view.findViewById(R.id.rv_produk)
+        val arrSlider = ArrayList<Int>()
+        arrSlider.add(R.drawable.slider1)
+        arrSlider.add(R.drawable.slider2)
+        arrSlider.add(R.drawable.slider3)
+        val adapterSlider = AdapterSlider(arrSlider, activity)
+        vpSlider.adapter=adapterSlider
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+
+        rvProduk.adapter = AdapterProduk(arrProduk)
+        rvProduk.layoutManager = layoutManager
+        return view
+    }
+    val arrProduk: ArrayList<Produk>get(){
+        val arr = ArrayList<Produk>()
+        val p1 = Produk()
+        p1.nama = "Hp core i3"
+        p1.harga = "RP.5.900000"
+        p1.gambar = R.drawable.asus1
+        val p2 = Produk()
+        p2.nama = "Hp core i3"
+        p2.harga = "RP.5.900000"
+        p2.gambar = R.drawable.asus2
+        val p3 = Produk()
+        p3.nama = "Hp core i3"
+        p3.harga = "RP.6.00988888"
+        p3.gambar = R.drawable.asus3
+        arr.add(p1)
+        arr.add(p2)
+        arr.add(p3)
+        return arr
     }
 
     companion object {

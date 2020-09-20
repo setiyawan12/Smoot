e3e3c95846c190489aa59c5e2d61214b609c0725
setiyawan12 <yayangsetiyawan0@gmail.com>
2020-09-20 23:16:56 +0700

    kotlin

--- app/src/main/java/com/example/smoot/MainActivity.kt
+++ app/src/main/java/com/example/smoot/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.example.smoot
 
+import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
+import com.example.smoot.activity.LoginActivity
 import com.example.smoot.fragment.AkunFragment
 import com.example.smoot.fragment.HomeFragment
 import com.example.smoot.fragment.KeranjangFragment
+import com.example.smoot.helper.SharedPref
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -22,9 +26,14 @@ class MainActivity : AppCompatActivity() {
     private lateinit var menu:Menu
     private lateinit var  menuItem:MenuItem
     private lateinit var  bottomNavigationView: BottomNavigationView
+
+    private var statusLogin = false
+
+    private lateinit var s: SharedPref
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        s= SharedPref(this)
         setUpBottomNav()
     }
     fun setUpBottomNav(){
@@ -48,7 +57,11 @@ class MainActivity : AppCompatActivity() {
 
                 }
                 R.id.navigation_akun -> {
-                    callFragment(2, fragmentAkun)
+                    if (s.getStatusLogin()){
+                        callFragment(2, fragmentAkun)
+                    }else{
+                        startActivity(Intent(this, LoginActivity::class.java))
+                    }
 
                 }
             }
